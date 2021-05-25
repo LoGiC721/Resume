fc216d37af0dabeb16fffe3c91dd8a124d2ab428
Abhishek Dhar <abhishekdhar1603@gmail.com>
2021-05-25 21:31:31 +0530

    stryled all pages now only templates remaining

--- Project/public/js/resumetemplate.js
+++ Project/public/js/resumetemplate.js
@@ -12,12 +12,15 @@ function pdf()
             background-image: url('images/a.jpg'); background-size: cover; background-position: top center; min-height: 620px;
           }
     </style>
-    <button class="btn btn-dark btn-lg" onclick="pdf()" 
+    <button class="btn btn-dark btn-lg text-light" onclick="pdf()" 
     onMouseOver="this.style.background='#E05343'"
     onMouseOut="this.style.background='#1C1F23'">Download PDF</button>
 <a class="btn btn-light btn-lg text-dark" style="margin-left:20px" href="/profile" 
-onMouseOver="this.style.background='#00BF85'"
+onMouseOver="this.style.background='#E05343'"
     onMouseOut="this.style.background='white'">Edit Details</a>
+    <a class="btn btn-dark btn-lg text-light" style="margin-left:20px ;background-color:#cf0000;" href="/" 
+    onMouseOver="this.style.background='#E05343'"
+        onMouseOut="this.style.background='#cf0000'">Back to Home page</a> 
     <br><br><br>
     </div>
     `;
