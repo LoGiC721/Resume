65125d5f27c557b6f0c72c5e1736538129bb9ccf
Abhishek Dhar <abhishekdhar1603@gmail.com>
2021-05-25 16:42:43 +0530

    styled download page

--- Project/public/js/resumetemplate.js
+++ Project/public/js/resumetemplate.js
@@ -3,9 +3,24 @@ function pdf()
 {
    document.getElementById("buttonn").remove();
     window.print();
-    var template=`<div id="body1" >
-    <button id="buttonn" type="submit" onclick="pdf()">Click</button>
-    </div>`;
+    var template=
+    `
+    <div id="buttonn" class="text-center">
+    <br><br><br>
+    <style>
+          body{
+            background-image: url('images/a.jpg'); background-size: cover; background-position: top center; min-height: 620px;
+          }
+    </style>
+    <button class="btn btn-dark btn-lg" onclick="pdf()" 
+    onMouseOver="this.style.background='#E05343'"
+    onMouseOut="this.style.background='#1C1F23'">Download PDF</button>
+<a class="btn btn-light btn-lg text-dark" style="margin-left:20px" href="/profile" 
+onMouseOver="this.style.background='#00BF85'"
+    onMouseOut="this.style.background='white'">Edit Details</a>
+    <br><br><br>
+    </div>
+    `;
     document.getElementById("body1").innerHTML=template;
     
 }
