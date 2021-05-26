ec628696ee6916af6d4e7853599193ba4568a801
Abhishek Dhar <abhishekdhar1603@gmail.com>
2021-05-26 20:17:09 +0530

    added responsiveness to pages

--- Project/public/js/resumetemplate.js
+++ Project/public/js/resumetemplate.js
@@ -12,13 +12,13 @@ function pdf()
             background: linear-gradient(to right,#23049d ,#cf0000 );
           }
     </style>
-    <button class="btn btn-dark btn-lg text-light" onclick="pdf()" 
+    <button class="btn btn-dark btn-lg text-light down" onclick="pdf()" 
     onMouseOver="this.style.background='#E05343'"
     onMouseOut="this.style.background='#1C1F23'">Download PDF</button>
-<a class="btn btn-light btn-lg text-dark" style="margin-left:20px" href="/profile" 
+<a class="btn btn-light btn-lg text-dark down" style="margin-left:20px" href="/profile" 
 onMouseOver="this.style.background='#E05343'"
     onMouseOut="this.style.background='white'">Edit Details</a>
-    <a class="btn btn-dark btn-lg text-light" style="margin-left:20px ;background-color:#cf0000;" href="/" 
+    <a class="btn btn-dark btn-lg text-light down" style="margin-left:20px ;background-color:#cf0000;" href="/" 
     onMouseOver="this.style.background='#E05343'"
         onMouseOut="this.style.background='#cf0000'">Back to Home page</a> 
     <br><br><br>
